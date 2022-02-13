fe5aaee42848d902736938a93f4802fa56eceee5
yaseenferoz <mdyaseenferoz@outlook.com>
2022-02-13 16:11:56 +0530

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {title: 'neat tree',
+    userName:'nature',
+    imageUrl:'assets/tree.jpeg',
+    content:'i saw this tree'
+  },
+    {title: 'Snowy Mountain tree',
+    userName:'mount lover',
+    imageUrl:'assets/mountain.jpeg',
+    content:'i saw this mountain'
+  },
+    {title: 'mountain biking',
+    userName:'mountain bikinbg',
+    imageUrl:'assets/biking.jpeg',
+    content:'i saw this biking'
+  },
+    {title: 'mountain biking',
+    userName:'mountain bikinbg',
+    imageUrl:'assets/biking.jpeg',
+    content:'i saw this biking'
+  
+  },
+    {title: 'mountain biking',
+    userName:'mountain bikinbg',
+    imageUrl:'assets/biking.jpeg',
+    content:'i saw this biking'
+  }
+  ,
+    {title: 'mountain biking',
+    userName:'mountain bikinbg',
+    imageUrl:'assets/biking.jpeg',
+    content:'i saw this biking'
+  }
+  ]
 }
